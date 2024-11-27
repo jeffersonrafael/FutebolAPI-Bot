b6486b312b387fd0a31f13d1c4bf2b2b5e4194de
jefferson rafael <contatojefferson25@proton.me>
2024-11-27 12:20:11 -0300

    update e mudanças

--- futebol-api/api.py
+++ futebol-api/api.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from requests.auth import HTTPBasicAuth
+import json
 
 api = "https://api.football-data.org/v4"
 url = f"{api}/matches"
@@ -9,10 +10,9 @@ headers = { 'X-Auth-Token': '24d761476ef14724a2593d62dfa91e57' }
 obj_api = requests.get(url, headers=headers)
 
 
-for match in obj_api.json()['matches']:
-  print(match)
+with open("partidas.json", 'w') as arquivo: 
+    json.dump(obj_api.json(), arquivo, indent=4)
 
-# if obj_api.status_code == 200:
-#     print(obj_api.json())
+
 
 
